2a95bcf16cdcc1df09617a099e93c3878b296253
CVH4OYC <166644659+CVH4OYC@users.noreply.github.com>
2024-09-26 12:17:04 +0500

    Добавление Authorize у GetAllAsync для теста

--- FoodsharingWebAPI/FoodsharingWebAPI/Controllers/BaseController.cs
+++ FoodsharingWebAPI/FoodsharingWebAPI/Controllers/BaseController.cs
@@ -24,6 +24,7 @@ namespace FoodsharingWebAPI.Controllers
         /// <summary>
         /// Метод для получения всех сущностей заданного типа
         /// </summary>
+        [Authorize]
         [HttpGet]
         public async Task<ActionResult<List<T>>> GetAllAsync(CancellationToken cancellationToken)
         {
