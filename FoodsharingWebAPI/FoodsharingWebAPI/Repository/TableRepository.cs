daeecd08a7af64d119c9f1264f388735692bd2d5
CVH4OYC <166644659+CVH4OYC@users.noreply.github.com>
2024-10-03 20:43:49 +0500

    Добавление экспорта в эксель для всех DbSetов
    Определяем имена DbSetов в контексте, для каждого из них получаем данные из БД, экспортируем эти данные на лист экселя

---
+++ FoodsharingWebAPI/FoodsharingWebAPI/Repository/TableRepository.cs
@@ -0,0 +1,40 @@
+﻿using DocumentFormat.OpenXml.InkML;
+using FoodsharingWebAPI.Data;
+using FoodsharingWebAPI.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Npgsql;
+using System.Data;
+
+namespace FoodsharingWebAPI.Repository
+{
+    public class TableRepository : ITableRepository
+    {
+        private readonly DbContext context;
+
+        public TableRepository(AppDbContext context)
+        {
+            this.context = context;
+        }
+        public async Task<DataTable> GetAllDataFromTableAsync(string tableName,CancellationToken cancellationToken)
+        {
+            
+            var dataTable = new DataTable(tableName);
+
+            var connection = context.Database.GetDbConnection();
+            await connection.OpenAsync(cancellationToken);
+
+            using (var command = connection.CreateCommand())
+            {
+                command.CommandText = $"SELECT * FROM \"{tableName}\""; 
+                using (var reader = await command.ExecuteReaderAsync(cancellationToken))
+                {
+                    dataTable.Load(reader); 
+                }
+            }
+
+            await connection.CloseAsync();
+            return dataTable;
+        }
+    }
+}
